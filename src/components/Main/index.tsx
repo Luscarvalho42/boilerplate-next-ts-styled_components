0522715b740cfc9344385d23bb28dd55eb28ecfb
luscarvalho42 <lucasdec.felizardo@gmail.com>
2023-06-17 21:16:00 -0300

    Finalizar boilerplate

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,11 @@
+import { Container } from './styles'
+
+const Main = () => {
+  return (
+    <Container>
+      <h1>Boilerplate</h1>
+    </Container>
+  )
+}
+
+export default Main
---
+++ src/components/Main/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Container = styled.main`
+  background-color: #de4049;
+  color: #fff;
+  width: 100%;
+  height: 100vh;
+  display: grid;
+  place-items: center;
+`
